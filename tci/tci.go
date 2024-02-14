a5c98e386bbdeaf0bde24adddd58cdf3d9caee15
Florian Thienel <ftl@thecodingflow.com>
2024-02-14 20:32:25 +0100

    move the parsing of the host address into a separate package for reuse

--- tci/tci.go
+++ tci/tci.go
@@ -2,13 +2,11 @@ package tci
 
 import (
 	"fmt"
-	"net"
-	"strconv"
-	"strings"
 	"time"
 
 	tci "github.com/ftl/tci/client"
 
+	"github.com/ftl/sdrainer/cli"
 	"github.com/ftl/sdrainer/dsp"
 	"github.com/ftl/sdrainer/rx"
 	"github.com/ftl/sdrainer/trace"
@@ -46,7 +44,7 @@ type Process struct {
 }
 
 func New(host string, trx int, mode rx.ReceiverMode, spotter Spotter, traceTCI bool) (*Process, error) {
-	tcpHost, err := parseTCPAddrArg(host, defaultHostname, defaultPort)
+	tcpHost, err := cli.ParseTCPAddrArg(host, defaultHostname, defaultPort)
 	if err != nil {
 		return nil, fmt.Errorf("invalid TCI host: %v", err)
 	}
@@ -260,48 +258,4 @@ func (l *tciListener) IQData(trx int, sampleRate tci.IQSampleRate, data []float3
 		end := begin + partLen
 		l.process.receiver.IQData(int(sampleRate), data[begin:end])
 	}
-}
-
-// TCP address handling
-
-func parseTCPAddrArg(arg string, defaultHost string, defaultPort int) (*net.TCPAddr, error) {
-	host, port := splitHostPort(arg)
-	if host == "" {
-		host = defaultHost
-	}
-	if port == "" {
-		port = strconv.Itoa(defaultPort)
-	}
-
-	return net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%s", host, port))
-}
-
-func splitHostPort(hostport string) (host, port string) {
-	host = hostport
-
-	colon := strings.LastIndexByte(host, ':')
-	if colon != -1 && validOptionalPort(host[colon:]) {
-		host, port = host[:colon], host[colon+1:]
-	}
-
-	if strings.HasPrefix(host, "[") && strings.HasSuffix(host, "]") {
-		host = host[1 : len(host)-1]
-	}
-
-	return
-}
-
-func validOptionalPort(port string) bool {
-	if port == "" {
-		return true
-	}
-	if port[0] != ':' {
-		return false
-	}
-	for _, b := range port[1:] {
-		if b < '0' || b > '9' {
-			return false
-		}
-	}
-	return true
 }
