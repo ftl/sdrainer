3f892672d98fafb37c549edf1eb714f476430f4b
Florian Thienel <ftl@thecodingflow.com>
2024-03-10 13:21:34 +0100

    define the Reporter interface that receives events from listeners and text processors (#3)

---
+++ rx/rx.go
@@ -0,0 +1,48 @@
+package rx
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/ftl/sdrainer/dsp"
+)
+
+type Reporter[F dsp.Number] interface {
+	ListenerActivated(listener string, frequency F)
+	ListenerDeactivated(listener string, frequency F)
+	CallsignDecoded(listener string, callsign string, frequency F, count int, weight int)
+	CallsignSpotted(listener string, callsign string, frequency F)
+	SpotTimeout(listener string, callsign string, frequency F)
+}
+
+type TextReporter[F dsp.Number] struct {
+	out io.Writer
+}
+
+func NewTextReporter[F dsp.Number](out io.Writer) *TextReporter[F] {
+	if out == nil {
+		out = os.Stdout
+	}
+	return &TextReporter[F]{out: out}
+}
+
+func (r *TextReporter[F]) ListenerActivated(listener string, frequency F) {
+	fmt.Fprintf(r.out, "listener %s activated on %.2fkHz\n", listener, float64(frequency))
+}
+
+func (r *TextReporter[F]) ListenerDeactivated(listener string, frequency F) {
+	fmt.Fprintf(r.out, "listener %s deactivated on %.2fkHz\n", listener, float64(frequency))
+}
+
+func (r *TextReporter[F]) CallsignDecoded(listener string, callsign string, frequency F, count int, weight int) {
+	fmt.Fprintf(r.out, "callsign %s decoded on %.2fkHz, heard %d times, weight is %d\n", listener, float64(frequency), count, weight)
+}
+
+func (r *TextReporter[F]) CallsignSpotted(listener string, callsign string, frequency F) {
+	fmt.Fprintf(r.out, "callsign %s spotted on %.2fkHz\n", listener, float64(frequency))
+}
+
+func (r *TextReporter[F]) SpotTimeout(listener string, callsign string, frequency F) {
+	fmt.Fprintf(r.out, "spot of %s on %.2fkHz timed out\n", listener, float64(frequency))
+}
